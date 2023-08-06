35c167f61e64309b465cf40a6b84399fb38da169
nikayel <alinikayeljamal@gmail.com>
2023-08-06 13:23:46 -0700

    add the new items url and views

--- item/forms.py
+++ item/forms.py
@@ -2,7 +2,14 @@ from django import forms
 
 from .models import Item
 
+INPUT_CLASSES = "w-full py-4 px-6 rounded-xl border"
 class NewItemForm(forms.ModelForm):
     class Meta:
         model = Item
         fields = ('catagory', 'name', 'description', 'price', 'image')
+        widgets = {
+            'catagory': forms.Select(attrs={'class': 'w-full py-4 px-6 rounded-xl border'}),
+            'name': forms.TextInput(attrs={'class': 'w-full py-4 px-6 rounded-xl border'}),
+            'price': forms.TextInput(attrs={'class': 'w-full py-4 px-6 rounded-xl border'}),
+            'description': forms.Textarea(attrs={'class': INPUT_CLASSES}),
+        }
--- item/views.py
+++ item/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
 from .forms import NewItemForm
 from .models import Item
@@ -15,5 +15,13 @@ def detail(request, pk):
     })
 
 def new(request):
-    form = NewItemForm()
+    if request.method == 'POST':
+        form = NewItemForm(request.POST, request.FILES)
+        if form.is_valid():
+            item = form.save(commit=False)
+            item.created_by = request.user
+            item.save()
+            return redirect('item:detail', pk=item.id)
+    else:
+         form = NewItemForm()
     return render(request, 'item/form.html', {'form': form, 'title': 'New item'})
